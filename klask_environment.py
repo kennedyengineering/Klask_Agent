593339573c9c844bc8239a1a763e1d3823e4e897
no <no>
2023-11-27 13:47:41 -0800

    refactored KeyboardController

--- klask_environment.py
+++ klask_environment.py
@@ -2,44 +2,14 @@
 
 from modules.Klask_Simulator.klask_simulator import KlaskSimulator
 from modules.Klask_Simulator.klask_constants import KG_BOARD_HEIGHT, KG_BOARD_WIDTH
+from KeyboardController import KeyboardController
 
 import pygame
 
 import numpy as np
 
-class KeyboardController():
-    __position_x = 0
-    __position_y = 0
-
-    def __init__(self, force):
-        self.force = force
-
-    def getAction(self):
-        return (self.__position_x * self.force, self.__position_y * self.force)
-
-    def keyUp_pressed(self):
-        self.__position_y += 1
-
-    def keyUp_released(self):
-        self.__position_y -= 1
-
-    def keyDown_pressed(self):
-        self.__position_y -= 1
-
-    def keyDown_released(self):
-        self.__position_y += 1
-
-    def keyLeft_pressed(self):
-        self.__position_x -= 1
-
-    def keyLeft_released(self):
-        self.__position_x += 1
-
-    def keyRight_pressed(self):
-        self.__position_x += 1
-
-    def keyRight_released(self):
-        self.__position_x -= 1
+# TODO: refactor into an enviornment class
+# TODO: define action space
 
 def numpy_to_pygame(arr):
     # Convert image stored in np array to pygame Surface
--- main.py
+++ main.py
@@ -1,42 +1,9 @@
 # Demonstrates a method of input for human players
 
 from modules.Klask_Simulator.klask_simulator import KlaskSimulator
+from KeyboardController import KeyboardController
 
 import pygame
-
-class KeyboardController():
-    __position_x = 0
-    __position_y = 0
-
-    def __init__(self, force):
-        self.force = force
-
-    def getAction(self):
-        return (self.__position_x * self.force, self.__position_y * self.force)
-
-    def keyUp_pressed(self):
-        self.__position_y += 1
-
-    def keyUp_released(self):
-        self.__position_y -= 1
-
-    def keyDown_pressed(self):
-        self.__position_y -= 1
-
-    def keyDown_released(self):
-        self.__position_y += 1
-
-    def keyLeft_pressed(self):
-        self.__position_x -= 1
-
-    def keyLeft_released(self):
-        self.__position_x += 1
-
-    def keyRight_pressed(self):
-        self.__position_x += 1
-
-    def keyRight_released(self):
-        self.__position_x -= 1
 
 # Initialize the simulator
 sim = KlaskSimulator(render_mode="human")
